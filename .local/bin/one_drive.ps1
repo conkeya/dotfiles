24a2f259566bfb0234e6cb2443bc65ab471d611a
Kevin Pietruszka <pietruszka@battelle.org>
2023-07-06 20:04:52 +0000

    clean up

--- .local/bin/one_drive.ps1
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env -S powershell.exe -ExecutionPolicy Bypass
-cd $env:OneDrive
-cd Documents
-echo $PWD
--- .local/make_secrets.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-$command='echo $env:OneDrive'
-$onedrivepath=$(powershell.exe -Command $command) | exit 1
-echo $onedrivepath
-
